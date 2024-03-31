9d3a546061dcb681c76d25fc3c595287fddeca4a
ValeriyCh04 <164645172+ValeriyCh04@users.noreply.github.com>
2024-03-31 17:32:42 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list = ['lada', 'audi', 'volvo', 'niva']
+print(my_list)
+print('First element:', my_list[0])
+print('Last element:', my_list[-1])
+print('Sublist:', my_list[2:5])
+my_list[2] = 'kia'
+print('Modified:', my_list)
+my_dict = {'lada': 'лада', 'audi': 'ауди', 'kia': 'киа', 'niva': 'нива'}
+print('Dictionary:', my_dict)
+print('Translation:', my_dict.values())
+print('Translation:', my_dict.keys())
+my_dict['volvo'] = 'вольво'
+print('Modified:', my_dict)
+
